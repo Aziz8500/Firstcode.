44b367be3b7b12d33b18b29c09e9dd90a3d66943
Acer <Acer@DESKTOP-7Q94UUR>
2021-02-06 14:21:29 +0300

    Final commit

--- Firstcode/src/printMyName.java
+++ Firstcode/src/printMyName.java
@@ -4,6 +4,7 @@ public class printMyName {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("My name is Abdulaziz");
+      System.out.println("this is my last name: alshahrani");
 	}
 
 }
